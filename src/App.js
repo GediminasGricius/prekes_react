b2f7187ef59794d233bb8905a59f3ef244c188b3
GediminasGricius <g.gricius@ituostas.lt>
2023-01-04 11:32:34 +0200

    up

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import Product from './components/product';
+import productsData from './data/productsData';
 
 function App() {
+  const prekes=productsData;
+ 
+  let products=[];
+  
+  for (let i=0; i<prekes.length; i++){
+    products.push(
+      <div class="col-md-4">
+        <Product product={prekes[i]}></Product>
+      </div>
+    );
+  }
+  
+ /*
+ prekes.forEach((product)=>{
+  products.push(
+    <div class="col-md-4">
+      <Product product={product}></Product>
+    </div>
+  );
+ });
+ */
+/*
+products=prekes.map((product)=>{
+  return (
+  <div class="col-md-4">
+    <Product product={product}></Product>
+  </div>)
+});
+*/
   return (
     <div class="container">
-      <div class="row mt-5">
-        <div class="col-md-4">
-          <div class="card text-bg-success mt-3">
-            <div class="card-header">PHP kursai</div>
-            <div class="card-body">
-              Čia išmoksite apie PHP
-              <button class='btn btn-success'>Plačiau</button>
-            </div>
-          </div>
+      <h3 class="mt-5">Specialus pasiūlymas</h3>
+      <div class="row mt-3">
+        <div class="col-md-6">
+          <Product product={prekes[1]}></Product>
         </div>
-        <div class="col-md-4">
-          <div class="card text-bg-info mt-3">
-              <div class="card-header">PHP kursai</div>
-              <div class="card-body">
-                Čia išmoksite apie JAVA
-                <button class='btn btn-success'>Plačiau</button>
-              </div>
-            </div>
+        <div class="col-md-6">
+          <Product product={prekes[2]}></Product>
         </div>
-        <div class="col-md-4 ">
-          <div class="card text-bg-warning mt-3">
-              <div class="card-header">PHP kursai</div>
-              <div class="card-body">
-                Čia išmoksite apie PHP
-                <button class='btn btn-success'>Plačiau</button>
-              </div>
-            </div>
-        </div>
+
+      </div>
+      <h3 class="mt-5">Visos prekės</h3>
+      <div class="row mt-3">
+        { products }
+        
       </div>
       
     </div> 
---
+++ src/data/productsData.js
@@ -0,0 +1,50 @@
+const productsData=[
+    {
+      title:"Skalbimo masina",
+      description:"Labai gera skalbimo masina",
+      price:250,
+      class:"text-bg-info"
+    },
+    {
+      title:"Indaplove",
+      description:"Labai gera indaplove ",
+      price:900,
+      class:"text-bg-info"
+    },
+    {
+      title:"Dziovykle",
+      description:"Labai gera skalbimo masina",
+      price:500,
+      class:"text-bg-success"
+    },
+    {
+      title:"Lova",
+      description:"Labai gera skalbimo masina",
+      price:200,
+      class:"text-bg-success"
+    },
+    {
+      title:"Virtuvės kėdė",
+      description:"Labai gera skalbimo masina",
+      price:50,
+      class:"text-bg-success"
+    },
+    {
+      title:"Televizorius",
+      description:"Labai gera skalbimo masina",
+      price:500,
+      class:"text-bg-success"
+    },
+    {
+      title:"Nešiojamas kompiuteris",
+      description:"Labai gera skalbimo masina",
+      price:1200,
+      class:"text-bg-danger"
+    }
+
+
+
+
+  ];
+
+  export default productsData;
